fa33137c8966b9851019217317c44c34d2f8ffcb
Saransh Sharma <saransh1337@gmail.com>
2019-06-04 16:46:15 +0530

    add rms sign in test

--- Simple1_test.js
+++ Simple1_test.js
@@ -10,7 +10,26 @@ Scenario('Is RMS online?!', (I) => {
     I.see('System');
   });
 
+
   Scenario('Is login button visible?', (I) => {
+    //Open RMS login page
     I.amOnPage(url);
     I.see('Sign In with Google');
+    I.click('Sign In with Google');
+    
+    //enter username, submit page
+    I.fillField(
+     '#view_container > div > div > div.pwWryf.bxPAYd > div > div.WEQkZc > div > form > span > section > div > span > div.cDSmF > div > div.aCsJod.oJeWuf > div > div.Xb9hP > div',
+     'saransh.sharma@shuttl.com');
+    I.click('#identifierNext > content > span');
+    I.wait(3);
+
+    //enter passwaord, submit page
+    I.fillField(
+      '#password > div.aCsJod.oJeWuf > div > div.Xb9hP > div',
+      // 'PASSWORD_HERE'
+      'gZ*IR$dwsH*G'
+    );
+    I.click('#passwordNext > content > span');
+    I.wait(3);
   });
