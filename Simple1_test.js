a8bdbe8a9ef2a52ebc9308cc393d56cc5a0316d0
Saransh Sharma <saransh1337@gmail.com>
2019-06-07 15:13:08 +0530

    refactor sign in

--- Simple1_test.js
+++ Simple1_test.js
@@ -1,6 +1,8 @@
 
 Feature('Do Simple Stuff');
-let url = "https://rms-admin.shuttlstage.com"
+let url = "https://rms-admin.shuttlstage.com";
+let userEmail = 'saransh.sharma@shuttl.com';
+let userPassword = 'gZ*IR$dwsH*G';
 
 Scenario('Is RMS online?!', (I) => {
     I.amOnPage(url);
@@ -8,28 +10,31 @@ Scenario('Is RMS online?!', (I) => {
     I.see('Route');
     I.see('Management');
     I.see('System');
+    I.say('RMS web portal is online !')
   });
 
 
-  Scenario('Is login button visible?', (I) => {
+  Scenario('Sign into RMS', (I) => {
     //Open RMS login page
     I.amOnPage(url);
     I.see('Sign In with Google');
     I.click('Sign In with Google');
+    I.say('Signing into RMS via gAuth');
     
     //enter username, submit page
+    I.say('Signing in with email: ')
     I.fillField(
      '#view_container > div > div > div.pwWryf.bxPAYd > div > div.WEQkZc > div > form > span > section > div > span > div.cDSmF > div > div.aCsJod.oJeWuf > div > div.Xb9hP > div',
-     'saransh.sharma@shuttl.com');
-    I.click('#identifierNext > content > span');
+     userEmail);
+    I.click('#identifierNext > span');
     I.wait(3);
 
     //enter passwaord, submit page
     I.fillField(
       '#password > div.aCsJod.oJeWuf > div > div.Xb9hP > div',
       // 'PASSWORD_HERE'
-      'gZ*IR$dwsH*G'
+      userPassword
     );
-    I.click('#passwordNext > content > span');
+    I.click('#passwordNext > span');
     I.wait(3);
   });
